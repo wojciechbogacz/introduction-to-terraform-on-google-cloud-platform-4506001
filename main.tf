8207e8dd7f0d9eb316fb3d8839ce16df679567ce
Wojciech Bogacz <72924796+wojciechbogacz@users.noreply.github.com>
2025-03-20 17:04:33 +0100

    net

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "google_compute_network" "app" {
 resource "google_compute_subnetwork" "app" {
   name          = "test-subnetwork"
   ip_cidr_range = "10.2.0.0/16"
-  region        = "us-central1-a"
+  region        = "us-central1"
   network       = google_compute_network.app.id
 }
 
