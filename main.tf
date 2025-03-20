812cdc9e68add355a89b1b786ad3c9798bdb20a9
Wojciech Bogacz <72924796+wojciechbogacz@users.noreply.github.com>
2025-03-20 22:36:27 +0100

    typo

--- main.tf
+++ main.tf
@@ -68,6 +68,6 @@ resource "google_compute_instance" "blog" {
     }
   }  
 
-  metadata_startup_script = "apt -y update"; "apt -y install nginx"; echo ${var.app_name} > /var/www/html/index.html"
+  metadata_startup_script = "apt -y update"; apt -y install nginx; echo ${var.app_name} > /var/www/html/index.html"
   allow_stopping_for_update = true
 }
